1cc595ff9e0572279c41e8a27270a701c729457d
Nando <nandopiless@gmail.com>
2024-10-15 17:55:44 +0200

    infinit loop to validate text

--- helpers.py
+++ helpers.py
@@ -4,3 +4,12 @@ import platform
 
 def clean_screen():
     os.system("cls") if platform.system() == "Windows" else os.system("clear")
+
+
+def read_text(length_min=0, length_max=100, message=None):
+    print(message) if message else None
+
+    while True:
+        text = input("> ")
+        if len(text) >= length_min and len(text) <= length_max:
+            return text
