c9c9b6c3e314b3bed0c65a7b73932c0f1e111e85
Nando <nandopiless@gmail.com>
2024-10-17 18:45:00 +0200

    decide if u want terminal or ui mode

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import sys
+import ui
 import menu
 
 if __name__ == "__main__":
-    menu.start()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.start()
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
