5dc6411b67061e000f42c112961f61470b404dc3
Nando <nandopiless@gmail.com>
2024-10-17 18:36:15 +0200

    modify client ui

--- ui.py
+++ ui.py
@@ -87,6 +87,72 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
         )
 
 
+class ModifyClientWindow(Toplevel, CenterWidgetMixin):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Modify client")
+        self.build()
+        self.centerWindow()
+        self.transient(parent)
+        self.grab_set()
+
+    def build(self):
+        frame = Frame(self)
+        frame.pack(padx=20, pady=10)
+
+        Label(frame, text="DNI (not editable)").grid(row=0, column=0)
+        Label(frame, text="Name (from 2 to 30 chars)").grid(row=0, column=1)
+        Label(frame, text="DNI (from 2 to 30 chars)").grid(row=0, column=2)
+
+        dni = Entry(frame)
+        dni.grid(row=1, column=0)
+        name = Entry(frame)
+        name.grid(row=1, column=1)
+        name.bind("<KeyRelease>", lambda event: self.validate(event, 0))
+        surname = Entry(frame)
+        surname.grid(row=1, column=2)
+        surname.bind("<KeyRelease>", lambda event: self.validate(event, 1))
+
+        client = self.master.treeView.focus()
+        fields = self.master.treeView.item(client, "values")
+        dni.insert(0, fields[0])
+        dni.config(state=DISABLED)
+        name.insert(0, fields[1])
+        surname.insert(0, fields[2])
+
+        frame = Frame(self)
+        frame.pack(pady=10)
+
+        modifyButton = Button(frame, text="Modify", command=self.modify_client)
+        modifyButton.grid(row=0, column=0)
+        Button(frame, text="Cancel", command=self.close).grid(row=0, column=1)
+
+        self.validations = [1, 1]  # all "True"
+        self.modifyButton = modifyButton
+        self.dni = dni
+        self.name = name
+        self.surname = surname
+
+    def modify_client(self):
+        client = self.master.treeView.focus()
+        self.master.treeView.item(client, values=(self.dni.get(), self.name.get(), self.surname.get()))
+        self.close()
+
+    def close(self):
+        self.destroy()
+        self.update()
+
+    def validate(self, event, index):
+        value = event.widget.get()
+        validation = value.isalpha() and 2 <= len(value) <= 30
+        event.widget.configure({"bg": "Green" if validation else "Red"})
+        # change button's status looking the validations
+        self.validations[index] = validation
+        self.modifyButton.config(
+            state=NORMAL if self.validations == [1, 1] else DISABLED
+        )
+
+
 class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
@@ -129,7 +195,7 @@ class MainWindow(Tk, CenterWidgetMixin):
         frame.pack(pady=20)
 
         Button(frame, text="Add", command=self.create).grid(row=0, column=0)
-        Button(frame, text="Modify", command=None).grid(row=0, column=1)
+        Button(frame, text="Modify", command=self.modify).grid(row=0, column=1)
         Button(frame, text="Delete", command=self.delete).grid(row=0, column=2)
 
         self.treeView = treeView  # export as an instance attribute
@@ -148,6 +214,10 @@ class MainWindow(Tk, CenterWidgetMixin):
 
     def create(self):
         CreateClientWindow(self)
+
+    def modify(self):
+        if self.treeView.focus():
+            ModifyClientWindow(self)
 
 
 if __name__ == "__main__":
